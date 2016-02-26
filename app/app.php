6afb5cca7767409a3994fbb39a57aedea771a982
Michael <michael.vang.1985@gmail.com>
2016-02-26 14:05:29 -0800

    add for loop and routing to add and display stylists to index.html.twig

--- app/app.php
+++ app/app.php
@@ -15,7 +15,13 @@
     ));
 
     $app->get("/", function () use ($app){
-        return $app['twig']->render('index.html.twig');
+        return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
+    });
+
+    $app->post("/", function () use ($app){
+        $new_stylist = new Stylist($_POST['stylist_name']);
+        $new_stylist->save();
+        return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
     });
 
     return $app;

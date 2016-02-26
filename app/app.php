f690ba2cdd61f5d7c68f2304a409ee442726e6fa
Michael <michael.vang.1985@gmail.com>
2016-02-26 13:43:22 -0800

    start app.php routing

--- app/app.php
+++ app/app.php
@@ -1,10 +1,11 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Inventory.php";
+    require_once __DIR__."/../src/Client.php";
+    require_once __DIR__."/../src/Stylist.php";
 
     $app = new Silex\Application();
 
-    $server = 'mysql:host=localhost;dbname=my_inventory';
+    $server = 'mysql:host=localhost;dbname=hair_salon';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
@@ -13,9 +14,11 @@
     'twig.path' => __DIR__.'/../views'
     ));
 
-    
+    $app->get("/", function () use ($app){
+        return $app['twig']->render('index.html.twig');
+    });
 
-      return $app;
+    return $app;
 
 
 ?>
